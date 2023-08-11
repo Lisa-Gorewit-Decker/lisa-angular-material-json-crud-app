b90385e6a5bd83388409d869c1e26ac44907fe73
Lisa Gorewit-Decker <50784106+lisagorewitdecker@users.noreply.github.com>
2023-08-11 13:47:02 -0700

    Create core.service.spec.ts

---
+++ src/app/core/core.service.spec.ts
@@ -0,0 +1,15 @@
+import {TestBed} from '@angular/core/testing';
+import {CoreService} from './core.service';
+
+describe('CoreService', () => {
+  let service: CoreService;
+
+  beforeEach(() => {
+    TestBed.configureTestingModule({});
+    service = TestBed.inject(CoreService);
+  });
+
+  it('should be created', () => {
+    expect(service).toBeTruthy();
+  });
+});
